390997d53a992edf64ad02e153676ea1dbae9f8e
AnilBamne <99521630+AnilBamne@users.noreply.github.com>
2022-11-08 08:15:38 +0530

    WelcomeToLambdaExpressionWithRegexProblems

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexWithLamdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda Expression problem");
+  
+        }
+    }
+}
